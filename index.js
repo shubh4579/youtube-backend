72a6ef40897d9dadd6097ee3d4067f44f8e4a6c8
Shubham Singh <shubh4579@gmail.com>
2025-09-07 21:16:52 +0530

    Fix corsee

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://youtube-frontend-liart.vercel.app/",
     credentials: true,
   })
 );
