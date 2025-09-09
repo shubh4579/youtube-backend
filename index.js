b82938899f50bf12bce6eafb42bdbfe5022cbfb1
Shubham Singh <shubh4579@gmail.com>
2025-09-09 13:05:38 +0530

    no trailing slash

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://youtube-frontend-liart.vercel.app/",
+    origin: "https://youtube-frontend-liart.vercel.app",
     credentials: true,
   })
 );
